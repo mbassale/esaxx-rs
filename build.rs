31adb28c95eabfa5549ddf4a028bc815e42acdcc
Marco Bassaletti <bassaletti@gmail.com>
2025-06-15 02:21:56 -0400

    [Vendor][esaxx-rs] Windows support for dynamic lib crt

--- build.rs
+++ build.rs
@@ -1,10 +1,21 @@
 #[cfg(feature = "cpp")]
-#[cfg(not(target_os = "macos"))]
+#[cfg(target_os = "linux")]
 fn main() {
     cc::Build::new()
         .cpp(true)
         .flag("-std=c++11")
         .static_crt(true)
+        .file("src/esaxx.cpp")
+        .include("src")
+        .compile("esaxx");
+}
+
+#[cfg(feature = "cpp")]
+#[cfg(target_os = "windows")]
+fn main() {
+    cc::Build::new()
+        .cpp(true)
+        .static_crt(false)
         .file("src/esaxx.cpp")
         .include("src")
         .compile("esaxx");
